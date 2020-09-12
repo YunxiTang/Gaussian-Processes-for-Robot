b21f80cd80167cd457acfac338fbb90ae5281bfe
YunxiTang <54171095+YunxiTang@users.noreply.github.com>
2020-09-12 12:37:21 +0800

    Add files via upload

--- Learn Estimated Dynamical Model Error/RR_Learn.m
+++ Learn Estimated Dynamical Model Error/RR_Learn.m
@@ -5,13 +5,14 @@ randn('seed',1);
 Memory_X = [];
 Yc1 = [];
 Yc2 = [];
-CAPACITY = 500;
+CAPACITY = 700;
 F_GP = [];
 SIG_1 = [];
 SIG_2 = [];
 MU_1 = [];
 MU_2 = [];
-
+rmse1 = [];
+rmse2 = [];
 dt = 0.01;
 
 % import robot model
@@ -26,7 +27,7 @@ model.wg = 1.2;
 
 % initialize the robot configuration
 x0 = [0.0; 0.0; deg2rad(0); deg2rad(0)];
-tspan = [0 50];
+tspan = [0 100];
 t_grid = linspace(tspan(1),tspan(2),(tspan(2)-tspan(1))/dt);
 
 % initialize the GP model (n GP Models)
@@ -50,11 +51,11 @@ hyp2_min = hyp2;
 %% initial state
 x = x0;
 mu = zeros(4,1);
-sigma = 0.1 * diag([0.5,0.5,1,1]);
+sigma = 0.01 * diag([0.5,0.5,1,1]);
 %% LEARN
 % ode options
 options = odeset('RelTol',1e-8, 'AbsTol',1e-8);
-xargu = [0;0;0;0;0;0;x(1);x(2);x(3);x(4)];
+xargu = [0;0;0;0;x(1);x(2)];
 Memory_X = [Memory_X xargu];
 Yc1 = [Yc1 0];
 Yc2 = [Yc2 0];
@@ -65,13 +66,20 @@ for i=1:length(t_grid)
     [qr,qrd,qrdd] = desiredJointTrajectory(t_current);
     
     % form of the collected data (column vector): x = [qrdd;qrd;q;qd] 
-    query_x = [qrdd(1);qrdd(2);qrd(1);qrd(2);qr(1);qr(2);
-               x(1);x(2);x(3);x(4)];
+    query_x = [qrdd(1);qrdd(2);qrd(1);qrd(2);
+               x(1);x(2)];
   
     % prediction
     [y1_mu, y1_s2, f1_mu, f1_s2] = gp(hyp1_min,@infGaussLik,meanfunc1,covfunc1,likfunc1,Memory_X',Yc1',query_x');
     [y2_mu, y2_s2, f2_mu, f2_s2] = gp(hyp2_min,@infGaussLik,meanfunc2,covfunc2,likfunc2,Memory_X',Yc2',query_x');
+    if abs(f1_mu) > 1 
+        f1_mu = sign(f1_mu) * 1;
+    end
+    if abs(f2_mu) > 1
+        f2_mu = sign(f2_mu) * 1;
+    end
     u = controlLaw_ComputedTorque(t_current,model,x,[],[1],[1]) + [f1_mu;f2_mu];
+
     SIG_1 = [SIG_1 sqrt(f1_s2)];
     SIG_2 = [SIG_2 sqrt(f2_s2)];
     MU_1 = [MU_1 f1_mu];
@@ -81,11 +89,11 @@ for i=1:length(t_grid)
     
     sol = ode45(@(t,x)(RRPlanarDynamics(model,x,u)), t_s, x, options);
     
-    t_s_grid = linspace(t_s(1),t_s(2),5);
+    t_s_grid = linspace(t_s(1),t_s(2),2);
     
     x_s = deval(sol,t_s_grid);
     
-    x_next = x_s(:,5);% + mvnrnd(mu,sigma)';
+    x_next = x_s(:,2)+ mvnrnd(mu,sigma)';
     
     X(:,i) = x_next;
     
@@ -101,40 +109,48 @@ for i=1:length(t_grid)
     Xr(:,i) = [qr(1);qr(2);qrd(1);qrd(2)];
     
     data_point_y = delta_tau;% + 0.01*gpml_randn(0.02,size(delta_tau'));
-    data_point_x = [qrdd(1);qrdd(2);qrd(1);qrd(2);qr(1);qr(2);
-                    x_next(1);x_next(2);x_next(3);x_next(4)];
+    data_point_x = [qrdd(1);qrdd(2);qrd(1);qrd(2);
+                    x_next(1);x_next(2)];
                 
     [D, n] = size(Memory_X);
-    if n <= CAPACITY
+    if n < CAPACITY
         disp('Not Enough Data!!! Collecting...');
         Memory_X = [Memory_X data_point_x];  % insert data directly
         Yc1 = [Yc1 data_point_y(1)];
         Yc2 = [Yc2 data_point_y(2)];
     elseif mod(i,20) == 0
         [Memory_X, indice] = randn_delete(Memory_X,'col');   % delete a column randnly
-        Memory_X = [Memory_X data_point_x];  % insert the new coming data
+        Memory_X(:,indice) = data_point_x;  % insert the new coming data
         
         Yc1 = certain_delete(Yc1,indice,'col');
         Yc2 = certain_delete(Yc2,indice,'col');
-        Yc1 = [Yc1 data_point_y(1)];
-        Yc2 = [Yc2 data_point_y(2)];
+        Yc1(:,indice) = data_point_y(1);
+        Yc2(:,indice) = data_point_y(2);
     end
     disp('-------------NEXT STATE!!!------------------');
     disp(t_current);
     % training ...
-    if mod(i,20) == 0
+    if mod(i,20) == 0 
         hyp1_min = minimize(hyp1,@gp,-5,@infGaussLik,meanfunc1,covfunc1,likfunc1,Memory_X',Yc1');
         hyp2_min = minimize(hyp2,@gp,-5,@infGaussLik,meanfunc2,covfunc2,likfunc2,Memory_X',Yc2');
     end
     % update x
     x = x_next;
     toc
+    if mod(i,1000)==0 && t_current > 0
+        e1 = ((Xr(1,:)-X(1,:)))/pi*180;
+        e2 = ((Xr(2,:)-X(2,:)))/pi*180;
+        RMSE_1 = sqrt((e1 * e1') / (t_current/dt));
+        RMSE_2 = sqrt((e2 * e2') / (t_current/dt));
+        rmse1 = [rmse1 RMSE_1];
+        rmse2 = [rmse2 RMSE_2];
+    end
 end
     
     
 %%
-e1 = (Xr(1,:)-X(1,:));
-e2 = (Xr(2,:)-X(2,:));
+e1 = ((Xr(1,:)-X(1,:)))/pi*180;
+e2 = ((Xr(2,:)-X(2,:)))/pi*180;
 figure(1);
 subplot(2,1,1);
 plot(t_grid,X(1,:),'-.',t_grid,Xr(1,:),'linewidth',2.0);
@@ -145,7 +161,7 @@ legend('q_2','q_{2d}');
 
 figure(2);
 subplot(2,1,1);
-plot(t_grid,e1,'linewidth',2.0);
+plot(t_grid,e1,'r','linewidth',2.0);
 title('Error_{q1}');
 grid on;
 subplot(2,1,2);
